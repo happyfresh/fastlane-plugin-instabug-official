9c07714a2294b796c229ec88dbc1bbe7b170077b
Ahmed AbouElhamayed <alwahsh.ahmed@gmail.com>
2019-07-30 09:27:47 +0200

    Beautify the code

--- lib/fastlane/plugin/instabug_official/actions/instabug_official_action.rb
+++ lib/fastlane/plugin/instabug_official/actions/instabug_official_action.rb
@@ -2,97 +2,95 @@ require 'fastlane/action'
 require_relative '../helper/instabug_official_helper'
 
 module Fastlane
-    module Actions
-        class InstabugOfficialAction < Action
-            def self.run(params)
-                api_token = params[:api_token]
+  module Actions
+    class InstabugOfficialAction < Action
+      def self.run(params)
+        api_token = params[:api_token]
 
-                endpoint = "https://api.instabug.com/api/sdk/v3/symbols_files"
-                command = "curl #{endpoint} --write-out %{http_code} --silent --output /dev/null -F os=\"ios\" -F application_token=\"#{api_token}\" -F symbols_file="
+        endpoint = 'https://api.instabug.com/api/sdk/v3/symbols_files'
+        command = "curl #{endpoint} --write-out %{http_code} --silent --output /dev/null -F os=\"ios\" -F application_token=\"#{api_token}\" -F symbols_file="
 
-                curlCommand = ""
-                single_path = params[:dsym_path]
+        curlCommand = ''
+        single_path = params[:dsym_path]
 
-                 if single_path != nil
-                    curlCommand += build_single_file_command(command, single_path)
-                 else
-                    dsym_paths = params[:dsym_array_paths].uniq
-                    dsym_paths.each do |file_path|
-                        curlCommand += build_single_file_command(command, file_path)
-                        if file_path != dsym_paths.last
-                            curlCommand += " | "
-                        end
+        if !single_path.nil?
+          curlCommand += build_single_file_command(command, single_path)
+        else
+          dsym_paths = params[:dsym_array_paths].uniq
+          dsym_paths.each do |file_path|
+            curlCommand += build_single_file_command(command, file_path)
+            curlCommand += ' | ' if file_path != dsym_paths.last
+          end
+        end
+
+        UI.verbose curlCommand
+        return curlCommand if Helper.test?
+
+        result = Actions.sh(curlCommand)
+        if result == '200'
+          UI.success 'dSYM is successfully uploaded to Instabug 🤖'
+        else
+          UI.error "Something went wrong during Instabug dSYM upload. Status code is #{result}"
+        end
+      end
+
+      def self.description
+        'upload dsyms to fastlane'
+      end
+
+      def self.authors
+        ['Karim_Mousa_89']
+      end
+
+      def self.return_value
+          # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.details
+        # Optional:
+        'upload dsyms to fastlane'
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :api_token,
+                                       env_name: 'FL_INSTABUG_API_TOKEN', # The name of the environment variable
+                                       description: 'API Token for Instabug', # a short description of this parameter
+                                       verify_block: proc do |value|
+                                                       UI.user_error!("No API token for InstabugAction given, pass using `api_token: 'token'`") unless value && !value.empty?
+                                                     end),
+          FastlaneCore::ConfigItem.new(key: :dsym_array_paths,
+                                       type: Array,
+                                       optional: true,
+                                       description: 'Array of paths to *.dSYM.zip files',
+                                       default_value: Actions.lane_context[SharedValues::DSYM_PATHS]),
+          FastlaneCore::ConfigItem.new(key: :dsym_path,
+                                       env_name: 'FL_INSTABUG_DSYM_PATH',
+                                       description: 'Path to *.dSYM file',
+                                       default_value: Actions.lane_context[SharedValues::DSYM_OUTPUT_PATH],
+                                       is_string: true,
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                                       UI.user_error!("dSYM file doesn't exists") unless File.exist?(value)
+                                                     end)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+        true
+      end
+
+      private
+
+      def self.build_single_file_command(command, dsym_path)
+        file_path = if dsym_path.end_with?('.zip')
+                      dsym_path.shellescape
+                    else
+                      ZipAction.run(path: dsym_path).shellescape
                     end
-                 end
-
-                UI.verbose curlCommand
-                return curlCommand if Helper.test?
-
-                result = Actions.sh(curlCommand)
-                if result == "200"
-                    UI.success 'dSYM is successfully uploaded to Instabug 🤖'
-                else
-                    UI.error "Something went wrong during Instabug dSYM upload. Status code is #{result}"
-                end
-            end
-
-def self.description
-"upload dsyms to fastlane"
-end
-
-def self.authors
-["Karim_Mousa_89"]
-end
-
-def self.return_value
-# If your method provides a return value, you can describe here what it does
-end
-
-def self.details
-# Optional:
-"upload dsyms to fastlane"
-end
-
-def self.available_options
-[
-FastlaneCore::ConfigItem.new(key: :api_token,
-                             env_name: 'FL_INSTABUG_API_TOKEN', # The name of the environment variable
-                             description: 'API Token for Instabug', # a short description of this parameter
-                             verify_block: proc do |value|
-                             UI.user_error!("No API token for InstabugAction given, pass using `api_token: 'token'`") unless value && !value.empty?
-                             end),
-FastlaneCore::ConfigItem.new(key: :dsym_array_paths,
-                             type: Array,
-                             optional: true,
-                             description: 'Array of paths to *.dSYM.zip files',
-                             default_value: Actions.lane_context[SharedValues::DSYM_PATHS]),
-FastlaneCore::ConfigItem.new(key: :dsym_path,
-                             env_name: 'FL_INSTABUG_DSYM_PATH',
-                             description: 'Path to *.dSYM file',
-                             default_value: Actions.lane_context[SharedValues::DSYM_OUTPUT_PATH],
-                             is_string: true,
-                             optional: true,
-                             verify_block: proc do |value|
-                                 UI.user_error!("dSYM file doesn't exists") unless File.exist?(value)
-                             end)
-]
-end
-
-def self.is_supported?(platform)
-platform == :ios
-true
-end
-
-private
-
-def self.build_single_file_command(command, dsym_path)
-if dsym_path.end_with?('.zip')
-    file_path = dsym_path.shellescape
-    else
-    file_path = ZipAction.run(path: dsym_path).shellescape
-end
-return command + "@\"#{file_path}\""
-end
-end
-end
+        command + "@\"#{file_path}\""
+      end
+    end
+  end
 end
